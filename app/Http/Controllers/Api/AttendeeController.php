abfe89184c190947efe6731070bea044f8572cfa
Your Name <nihaal@sapnagroup.com>
2024-02-11 20:10:56 +0100

    Authorization with gates

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -14,6 +14,11 @@ class AttendeeController extends Controller
     use CanLoadRelationships;
     private array $relations = ['user'];
 
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+    }
+
     public function index(Event $event)
     {
         $attendees = $this->loadRelationships(
@@ -48,8 +53,9 @@ class AttendeeController extends Controller
         //
     }
 
-    public function destroy(string $event, Attendee $attendee)
+    public function destroy(Event $event, Attendee $attendee)
     {
+        $this->authorize('delete-attendee', [$event, $attendee]);
         $attendee->delete();
 
         return response(status: 204);
